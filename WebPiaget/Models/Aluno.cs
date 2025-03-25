3c1bed75a39b236be89f5f49479cb0905772c2b0
vini-0123 <sonicdahora@gmail.com>
2025-03-25 11:03:04 -0300

    Adicionar arquivos de projeto.

---
+++ WebPiaget/Models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace WebPiaget.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Professor { get; set; }
+        public DateTime Idade { get; set; }
+    }
+}
